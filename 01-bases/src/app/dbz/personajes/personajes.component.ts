163c34a4b2cdc6b135f43d499af70fc6625fdf33
RICHARD AGUILERA <richard.aguilera09@gmail.com>
2023-02-26 11:31:21 -0300

    Fin Sección 7 - Aplicación para buscar gifs

--- 01-bases/src/app/dbz/personajes/personajes.component.ts
+++ 01-bases/src/app/dbz/personajes/personajes.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { Personaje } from '../interfaces/dbz.interface';
+import { Component } from '@angular/core';
 import { DbzService } from '../services/dbz.service';
 
 @Component({
@@ -7,8 +6,6 @@ import { DbzService } from '../services/dbz.service';
   templateUrl: './personajes.component.html'
 })
 export class PersonajesComponent {
-
-  //@Input() personajes: Personaje[] = [];
 
   get personajes(){
     return this.dbzService.personajes;
